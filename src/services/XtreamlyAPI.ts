3614c402098a1775bbc650a29b9d62170d19b84b
panosdb <ppsimatikas@gmail.com>
2025-02-18 14:33:59 +0200

    Add volatility prediction API x-api-key

--- src/services/XtreamlyAPI.ts
+++ src/services/XtreamlyAPI.ts
@@ -7,9 +7,24 @@ export enum XtreamlyAPIPath {
 
 export class XtreamlyAPI {
   private baseUrl: string
+  private headers: Headers
 
   constructor() {
     this.baseUrl = "https://api.xtreamly.io/";
+
+    const XTREAMLY_API_KEY = process.env.XTREAMLY_API_KEY;
+
+    if (!XTREAMLY_API_KEY) {
+      throw new Error(`
+        Missing environment variables: XTREAMLY_API_KEY.
+        Request your API key here: https://xtreamly.io/api 
+      `);
+    }
+
+    this.headers = new Headers({
+      "Content-Type": "application/json",
+      "x-api-key": XTREAMLY_API_KEY,
+    });
   }
 
   async get(path: XtreamlyAPIPath, params?: Record<string, any>): Promise<any> {
@@ -19,6 +34,9 @@ export class XtreamlyAPI {
       Object.entries(params).forEach(([key, value]) => url.searchParams.append(key, value));
     }
 
-    return fetch(url.toString()).then(res => res.json())
+    return fetch(url.toString(),{
+      method: "GET",
+      headers: this.headers,
+    }).then(res => res.json())
   }
 }
